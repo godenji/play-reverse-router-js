7414de4968d01a09f0f9446a204705e4665fd7f0
godenji <admin@newmindit.com>
2013-07-09 18:59:24 +0200

    include updated sample routes with full package path

--- public/javascripts/routes-all.js
+++ public/javascripts/routes-all.js
@@ -5,32 +5,32 @@ var _qS = jRouter.qString
 var _s 	= jRouter.secure
 var _wA = jRouter.route  
 
-_nS('Foo.qString');
-_root.Foo.qString =
+_nS('foo.Foo.qString');
+_root.foo.Foo.qString =
 
       function(arg1,arg2) {
       return _wA({method:"GET", url:"/foo/" + "qstring" + _qS([(function(k,v){return v!=null?(function(k,v) {return encodeURIComponent(k)+'='+encodeURIComponent(v)})(k,v):''})("arg1", arg1), (function(k,v){return v!=null?(function(k,v) {return encodeURIComponent(k)+'='+encodeURIComponent(v)})(k,v):''})("arg2", arg2)])});
       };
    
 
-_nS('Foo.index');
-_root.Foo.index =
+_nS('foo.Foo.index');
+_root.foo.Foo.index =
 
       function() {
       return _wA({method:"GET", url:"/foo/" + "index"});
       };
    
 
-_nS('Bar.qString');
-_root.Bar.qString =
+_nS('bar.Bar.qString');
+_root.bar.Bar.qString =
 
       function(arg1,arg2) {
       return _wA({method:"GET", url:"/bar/" + "qstring" + _qS([(function(k,v){return v!=null?(function(k,v) {return encodeURIComponent(k)+'='+encodeURIComponent(v)})(k,v):''})("arg1", arg1), (function(k,v){return v!=null?(function(k,v) {return encodeURIComponent(k)+'='+encodeURIComponent(v)})(k,v):''})("arg2", arg2)])});
       };
    
 
-_nS('Bar.index');
-_root.Bar.index =
+_nS('bar.Bar.index');
+_root.bar.Bar.index =
 
       function() {
       return _wA({method:"GET", url:"/bar/" + "index"});
