ca63b407fa71e4aca940d6c02d117a1934a49040
godenji <admin@newmindit.com>
2013-12-22 11:30:13 +0000

    remove extranous refs to global reverse router (i.e. github project for handling play server-side routes)

--- module/bar/app/controllers/Bar.scala
+++ module/bar/app/controllers/Bar.scala
@@ -3,8 +3,6 @@ package controllers.bar
 import play.api._
 import play.api.mvc._
 
-import com.company.utils.router
-
 object Bar extends Controller {
   
   def index = Action {
@@ -12,8 +10,7 @@ object Bar extends Controller {
   }
   
   def qString(arg1: Option[Int], arg2: Option[String]) = Action {
-  	val uri = router.foo.FooQString(arg1,arg2)
-  	Ok("Bar doesn't depend on Foo but it can still link to it: "+uri)
+  	Ok("Bar js route should support query string")
   }
 
 }
--- module/foo/app/controllers/Foo.scala
+++ module/foo/app/controllers/Foo.scala
@@ -3,8 +3,6 @@ package controllers.foo
 import play.api._
 import play.api.mvc._
 
-import com.company.utils.router
-
 object Foo extends Controller {
 
 	def index = Action {
@@ -12,8 +10,7 @@ object Foo extends Controller {
   }
 
   def qString(arg1: Option[Int], arg2: Option[String]) = Action {
-  	val uri = router.bar.BarQString(arg1,arg2)
-  	Ok("Foo doesn't depend on Bar but it can still link to it: "+uri)
+		Ok("Foo js route should support query string")
   }
 
 }
